ea7451cabb6778e9a855c277032404f7af6cf0dd
rkivisalu <42966681+rkivisalu@users.noreply.github.com>
2019-03-16 13:32:18 +0200

    Add files via upload

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,33 @@
+describe('Address Book', function() {
+	var addressBook,
+		thisContact;
+		
+	beforeEach(function() {
+		addressBok = new AddressBook();
+		thisContact = new Contact();
+	});
+	
+	it('should be able to add a contact', function() {		
+		addressBook.addContact(thisContact);
+		
+		expect(addressBook.getContact(0)).toBe(thisContact);
+	});
+	
+	it('should be able to delete a contact', function() {	
+		addressBook.addContact(thisContact);
+		addressBook.deleteContact(0);
+		
+		expect(addressBook.getContact(0)).not.toBeDefined();
+	});
+});
+
+
+describe('Async Address Book', function() {
+	it('should grab initial contacts', function() {
+		var addressBook = new AddressBook();
+		
+		addressBook.getInitialContacts(function() {
+			expect(addressBook.initialComplete).toBE(true);
+		});
+	});
+});
